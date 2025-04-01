681a23ffb25f1ec0af354329094b245468ddaf16
VRINDA GUPTA <vrindag.ug23.cs@nitp.ac.in>
2025-04-01 13:56:02 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   const loadData=async () =>{
     console.log('Loading Data...');
-    let response=await fetch("http://localhost:5000/api/foodData",{
+    let response=await fetch("backend-mernapp.onrender.com/api/foodData",{
       method:"POST",
       headers:{
         'Content-Type':'application/json',
