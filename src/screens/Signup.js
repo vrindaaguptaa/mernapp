fc1deaa17238afe01b9bcfae43e020e2101ca0b4
VRINDA GUPTA <vrindag.ug23.cs@nitp.ac.in>
2025-04-01 13:56:57 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -5,7 +5,7 @@ export default function Signup() {
 
   const handleSubmit=async(e) => {
      e.preventDefault();
-     const response=await fetch("http://localhost:5000/api/createuser",{
+     const response=await fetch("backend-mernapp.onrender.com/api/createuser",{
       method:'POST',
       headers:{
         'Content-Type':'application/json'
